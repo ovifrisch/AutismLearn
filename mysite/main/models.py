92421ae0897b79417f2a22e9c7d783b68b00d25a
Ovi Fritsch <oafritsch@ucdavis.edu>
2019-02-09 17:51:27 -0800

    init django and make basic models

---
+++ mysite/main/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.conf import settings
+
+class Teacher(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Student(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Class(models.Model):
+    name = models.CharField(max_length=50)
+    students = models.ManyToManyField(Student)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, default=None)
+
+
+    def __str__(self):
+        return self.name
+
+class StudentInterest(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Assignment(models.Model):
+    name = models.CharField(max_length=50)
+    subject = models.CharField(max_length=50)
+    _class = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class GenericQuestion(models.Model):
+    number = models.IntegerField()
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    string_representation = models.TextField()
+
+    def __str__(self):
+        return self.string_representation
+
+
+class UniqueQuestion(models.Model):
+    generic_question = models.ForeignKey(GenericQuestion, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    string_representation = models.TextField()
+
+    def __str__(self):
+        return self.string_representation
