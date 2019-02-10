d5c145b6bfdb38ac34be148907142ae133c76b6f
Ovi Fritsch <oafritsch@ucdavis.edu>
2019-02-09 18:57:52 -0800

    baseline for views

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Class
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Class)
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(StudentInterest)
+admin.site.register(Assignment)
+admin.site.register(GenericQuestion)
+admin.site.register(UniqueQuestion)
+
+
+#
+# for model in get_models(get_app('main')):
+#     admin.site.register(model)
