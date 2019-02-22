ed76ff2f80f792a5a9359a7afb97cc92548fd23e
Ovi Fritsch <oafritsch@ucdavis.edu>
2019-02-22 15:11:28 -0800

    debug false and add url to allowed_hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,9 +28,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SECRET_KEY = '6yhgtxhwz5191(_o!+2q&+#-5)&%u7cx4r&-vc+pstr@0nzvtj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lit-island-28681.herokuapp.com']
 
 
 # Application definition
